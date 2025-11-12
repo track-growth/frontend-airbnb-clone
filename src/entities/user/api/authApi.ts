90428cacd0906031c177e1a574ceb4f694a097e6
yeon <xxyeon129@gmail.com>
2025-11-13 02:57:34 +0900

    #edit-login-endpoint Style: 로그인 API 요청 endpoint 수정, signUpSchema 보완, index 경로 수정

--- src/entities/user/api/authApi.ts
+++ src/entities/user/api/authApi.ts
@@ -5,20 +5,20 @@
  */
 
 // entities layers
-import type { SignUpRequest, SignUpResponse, LoginRequest, LoginResponse } from '../model'
+import type { SignUpRequest, SignUpResponse, LoginRequest, LoginResponse } from '../model';
 // shared layers
-import { axiosInstance } from '@/shared'
+import { axiosInstance } from '@/shared';
 
 export const authApi = {
   signUp: async (data: SignUpRequest): Promise<SignUpResponse> => {
-    const response = await axiosInstance.post('/api/members/signup', data)
-    return response.data
+    const response = await axiosInstance.post('/api/members/signup', data);
+    return response.data;
   },
   // NOTE: 로그인 API
   // - props(data) type으로 email, password를 받는다.
   // - axiosInstance를 사용하여 API 호출
   login: async (data: LoginRequest): Promise<LoginResponse> => {
-    const response = await axiosInstance.post('/api/members/login', data)
-    return response.data
+    const response = await axiosInstance.post('/api/auth/login', data, { withCredentials: true });
+    return response.data;
   },
-}
+};
