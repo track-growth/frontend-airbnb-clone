508517c57e9c669d482249a2182a176648604427
yeon <xxyeon129@gmail.com>
2025-11-13 02:57:34 +0900

    #edit-login-endpoint Style: 로그인 API 요청 endpoint 수정, signUpSchema 보완, index 경로 수정

--- src/entities/user/model/signup.types.ts
+++ src/entities/user/model/signup.types.ts
@@ -8,7 +8,7 @@ import { z } from 'zod';
 // NOTE: 회원가입 form zod schema
 export const signUpSchema = z
   .object({
-    email: z.email('올바른 이메일 형식을 입력해주세요'),
+    email: z.email('올바른 이메일 형식을 입력해주세요').min(1, '이메일을 입력해주세요').trim(),
     password: z
       .string()
       .min(8, '비밀번호는 최소 8자 이상이어야 합니다')
@@ -16,13 +16,15 @@ export const signUpSchema = z
       .regex(
         /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]/,
         '영문, 숫자, 특수문자를 포함해야 합니다',
-      ),
-    confirmPassword: z.string().min(1, '비밀번호 확인을 입력해주세요'),
+      )
+      .trim(),
+    confirmPassword: z.string().min(1, '비밀번호 확인을 입력해주세요').trim(),
     name: z
       .string()
       .min(1, '이름을 입력해주세요')
       .min(2, '이름은 최소 2자 이상이어야 합니다')
-      .max(20, '이름은 최대 20자까지 가능합니다'),
+      .max(20, '이름은 최대 20자까지 가능합니다')
+      .trim(),
   })
   .refine(data => data.password === data.confirmPassword, {
     message: '비밀번호가 일치하지 않습니다',
